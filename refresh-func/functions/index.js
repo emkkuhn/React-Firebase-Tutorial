567603c710f955170209b6d78de5f8041cd7326b
Emily Kuhn <emkkuhn@umich.edu>
2020-08-27 21:53:52 -0700

    safety and notifications

--- refresh-func/functions/index.js
+++ refresh-func/functions/index.js
@@ -1,9 +1,9 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
-
-
 const FBAuth = require('./util/fbAuth');
+
+const { db } = require('./util/admin');
 
 const{ getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/screams');
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
@@ -29,7 +29,6 @@ app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 app.get('/scream/:screamId/like', FBAuth, likeScream);
 app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.delete('/scream/:screamId', FBAuth, deleteScream);
-//TODO delete scream
 
 //users routes
 app.post('/signup', signup);
@@ -40,3 +39,59 @@ app.get('/user', FBAuth, getAuthenticatedUser);
 
 
 exports.api = functions.https.onRequest(app);
+exports.createNotificationOnLike = functions.firestore.document('likes/{id}').onCreate((snapshot) => {
+	db.doc(`/screams/${snapshot.data().screamId}`).get()
+		.then(doc => {
+			if(doc.exists){
+				return db.doc(`/notifications/${snapshot.id}`).set({
+					createdAt: new Date().toISOString(),
+					recipient: doc.data().userHandle,
+					senter: snapshot.data().userHandle,
+					type: 'like',
+					read: 'false',
+					screamId: doc.id
+				})
+			}
+		})
+		.then(() => {
+			return;
+		})
+		.catch(err => {
+			console.error(err);
+			return;
+		});
+});
+
+exports.deleteNotificationOnUnlike = functions.firestore.document('likes/{id}').onDelete((snapshot) => {
+	db.doc(`/notifications/${snapshot.id}`).delete()
+		.then(() => {
+			return;
+		})
+		.catch(err => {
+			console.error(err);
+			return;
+		})
+});
+
+exports.createNotificationOnComment = functions.firestore.document('comments/{id}').onCreate((snapshot) => {
+	db.doc(`/screams/${snapshot.data().screamId}`).get()
+		.then(doc => {
+			if(doc.exists){
+				return db.doc(`/notifications/${snapshot.id}`).set({
+					createdAt: new Date().toISOString(),
+					recipient: doc.data().userHandle,
+					senter: snapshot.data().userHandle,
+					type: 'comment',
+					read: 'false',
+					screamId: doc.id
+				})
+			}
+		})
+		.then(() => {
+			return;
+		})
+		.catch(err => {
+			console.error(err);
+			return;
+		});
+});
--- refresh-func/functions/util/admin.js
+++ refresh-func/functions/util/admin.js
@@ -1,12 +1,17 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("/Users/User/Desktop/refresh-24c1c-firebase-adminsdk-95x8z-86f7b37b0a.json");
+ // export GOOGLE_APPLICATION_CREDENTIALS="/Users/User/Desktop/refresh-24c1c-firebase-adminsdk-95x8z-86f7b37b0a.json"
+//var serviceAccount = require("/Users/User/Desktop/refresh-24c1c-firebase-adminsdk-95x8z-86f7b37b0a.json");
 
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://refresh-24c1c.firebaseio.com",
+//   storageBucket: "refresh-24c1c.appspot.com"
+// });
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.applicationDefault(),
   databaseURL: "https://refresh-24c1c.firebaseio.com",
   storageBucket: "refresh-24c1c.appspot.com"
 });
-
 const db = admin.firestore();
 
 module.exports = { admin, db };
